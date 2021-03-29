c374ceed77100f3d202c812dcd2378f79ca963cc
Vinicius Augusto <vinicius_augusto_@msn.com>
2021-03-29 16:07:51 -0300

    Implementing services

---
+++ src/main/java/com/viniciusaugusto/beerapi/controller/BeerController.java
@@ -0,0 +1,50 @@
+package com.viniciusaugusto.beerapi.controller;
+
+import com.viniciusaugusto.beerapi.dto.BeerDTO;
+import com.viniciusaugusto.beerapi.dto.QuantityDTO;
+import com.viniciusaugusto.beerapi.exception.BeerAlreadyRegisteredException;
+import com.viniciusaugusto.beerapi.exception.BeerNotFoundException;
+import com.viniciusaugusto.beerapi.exception.BeerStockExceededException;
+import com.viniciusaugusto.beerapi.service.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/beers")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class BeerController implements BeerControllerDocs {
+
+    private final BeerService beerService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BeerDTO createBeer(@RequestBody @Valid BeerDTO beerDTO) throws BeerAlreadyRegisteredException {
+        return beerService.createBeer(beerDTO);
+    }
+
+    @GetMapping("/{name}")
+    public BeerDTO findByName(@PathVariable String name) throws BeerNotFoundException {
+        return beerService.findByName(name);
+    }
+
+    @GetMapping
+    public List<BeerDTO> listBeers() {
+        return beerService.listAll();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws BeerNotFoundException {
+        beerService.deleteById(id);
+    }
+
+    @PatchMapping("/{id}/increment")
+    public BeerDTO increment(@PathVariable Long id, @RequestBody @Valid QuantityDTO quantityDTO) throws BeerNotFoundException, BeerStockExceededException {
+        return beerService.increment(id, quantityDTO.getQuantity());
+    }
+}
